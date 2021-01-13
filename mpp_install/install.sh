5043c8aef4b4dbc8b729a51e0a397f7326d64e6a
unknown <shuai.jiang@unistrong.com>
2021-01-13 20:28:39 +0800

    new install script

--- mpp_install/install.sh
+++ mpp_install/install.sh
@@ -1,6 +1,8 @@
 #################################################################
 ## Global declaration                                          ##
 #################################################################
+HOSTFILE=/opt/all_host
+SEGFILE=/opt/all_segment
 TARNAME="gpdb*.tar.gz"
 CUSTOMTAR="false"
 MPPVERSION="BeyonDB MPP v6.0.0" #unimplement
@@ -14,19 +16,10 @@ MASDIR=/home/$USERNAME/gpdata/gpmaster
 PRIDIR=/home/$USERNAME/gpdata/gpdatap
 MRRDIR=/home/$USERNAME/gpdata/gpdatam
 NUMSEG=1 #in most scenario, one server should install 1~4 segments, so we need 1~4 PRIDIR and MRRDIR variables.
-         #uninplement
 
-#################################################################
-## Input options                                               ##
-#################################################################
 case $1 in
-        "-P" )
-        TARNAME=$2
-        CUSTOMTAR="true"
-        ;;
-        "-p" )
-        TARNAME=$2
-        CUSTOMTAR="false"
+        "-clean" )
+        CLEAN="f"
         ;;
         "--help" | "-help" | "-h" )
         echo "MPPinstaller.sh use to config system configuration parameters, before using this tool"
@@ -60,15 +53,9 @@ case $1 in
         "-n" | "--numseg") #unimplement
         NUMSEG=$2
 
-esac 
+esac
 
-if [ $INSTALLMODE == "debug" ] ; then
-echo "[DEBUG]:-Variable CUSTOMTAR is $CUSTOMTAR."
-fi
 
-#################################################################
-## get username                                                ##
-#################################################################
 hncheck()
 {
  if [ $hnflag == 'y' ] ; then
@@ -80,9 +67,6 @@ hncheck()
 fi 
 }
 
-#################################################################
-## get mirror                                                  ##
-#################################################################
 mrcheck()
 {
  if [ $MIRRORFLAG == 'y' ] ; then
@@ -94,39 +78,43 @@ mrcheck()
 fi 
 }
 
-#################################################################
-## function press a key to continue                            ##
-#################################################################
-get_char()
+withmirror()
 {
-    SAVEDSTTY=`stty -g`
-    stty -echo
-    stty cbreak
-    dd if=/dev/tty bs=1 count=1 2> /dev/null
-    stty -raw
-    stty echo
-    stty $SAVEDSTTY
+cat>>/home/$USERNAME/gpinitsystem_config<<EOF
+ARRAY_NAME="BeyonDB MPP"
+SEG_PREFIX=gpseg
+PORT_BASE=40000
+declare -a DATA_DIRECTORY=(/home/$USERNAME/gpdata/gpdatap)
+MASTER_HOSTNAME=master
+MASTER_DIRECTORY=/home/$USERNAME/gpdata/gpmaster
+MASTER_PORT=5432
+TRUSTED_SHELL=ssh
+CHECK_POINT_SEGMENTS=8
+ENCODING=UNICODE
+MIRROR_PORT_BASE=50000
+REPLICATION_PORT_BASE=41000
+MIRROR_REPLICATION_PORT_BASE=51000
+declare -a MIRROR_DATA_DIRECTORY=(/home/$USERNAME/gpdata/gpdatam)
+EOF
 }
 
-#################################################################
-## Make the install script repeatable.                         ##
-#################################################################
-
-backup()  
+basic()
 {
-ssh -t -Tq root@$line "rm -rf /etc/sysctl.bak"
-ssh -t -Tq root@$line "cp /etc/sysctl.conf /etc/sysctl.bak"
-ssh -t -Tq root@$line "cat /dev/null > /etc/sysctl.conf"
-ssh -t -Tq root@$line "rm -rf /etc/security/limits.bak"
-ssh -t -Tq root@$line "cp /etc/security/limits.conf /etc/security/limits.bak"
-ssh -t -Tq root@$line "cat /dev/null > /etc/security/limits.conf"
+cat>>/home/$USERNAME/gpinitsystem_config<<EOF
+ARRAY_NAME="BeyonDB MPP"
+SEG_PREFIX=gpseg
+PORT_BASE=40000
+declare -a DATA_DIRECTORY=(/home/$USERNAME/gpdata/gpdatap)
+MASTER_HOSTNAME=master
+MASTER_DIRECTORY=/home/$USERNAME/gpdata/gpmaster
+MASTER_PORT=5432
+TRUSTED_SHELL=ssh
+CHECK_POINT_SEGMENTS=8
+ENCODING=UNICODE
+EOF
 }
 
-#################################################################
-## Modify sysctl.conf.                                         ##
-#################################################################
-
-sysctlMod()     
+sysctlMod()
 {
 ssh -t -Tq root@$line "cat>>/etc/sysctl.conf<<EOF
 kernel.shmmax = 500000000
@@ -153,10 +141,6 @@ EOF"
 ssh -t -Tq root@$line "sysctl -p >> install_log.txt 2>> install_error.txt"  
 }
 
-#################################################################
-## Modify limits.conf                                          ##
-#################################################################
-
 limitMod()
 {
 ssh -t -Tq root@$line "cat>>/etc/security/limits.conf<<EOF
@@ -168,58 +152,28 @@ ssh -t -Tq root@$line "cat>>/etc/security/limits.conf<<EOF
 EOF"
 }
 
-#################################################################
-## Change system security policy                               ##
-#################################################################
-
 secMod()
 {
 ssh -t -Tq root@$line "sed -i "s/enforcing/disabled/g" /etc/selinux/config"
-ssh -t -Tq root@$line "systemctl stop firewalld >> install_log.txt 2>> install_error.txt"
-ssh -t -Tq root@$line "systemctl disable firewalld >> install_log.txt 2>> install_error.txt"
 }
 
-#################################################################
-## gpinitsystem basic                                          ##
-#################################################################
-basic()
+closefirewall()
 {
-cat>>/home/$USERNAME/gpinitsystem_config<<EOF
-ARRAY_NAME="BeyonDB MPP"
-SEG_PREFIX=gpseg
-PORT_BASE=40000
-declare -a DATA_DIRECTORY=(/home/$USERNAME/gpdata/gpdatap)
-MASTER_HOSTNAME=master
-MASTER_DIRECTORY=/home/$USERNAME/gpdata/gpmaster
-MASTER_PORT=5432
-TRUSTED_SHELL=ssh
-CHECK_POINT_SEGMENTS=8
-ENCODING=UNICODE
-EOF
+  ssh -t -Tq root@$line "systemctl stop firewalld >> install_log.txt 2>> install_error.txt"
+  ssh -t -Tq root@$line "systemctl disable firewalld >> install_log.txt 2>> install_error.txt"  
 }
 
-#################################################################
-## gpinitsystem with mirror                                    ##
-#################################################################
-withmirror()
+get_char()
 {
-cat>>/home/$USERNAME/gpinitsystem_config<<EOF
-ARRAY_NAME="BeyonDB MPP"
-SEG_PREFIX=gpseg
-PORT_BASE=40000
-declare -a DATA_DIRECTORY=(/home/$USERNAME/gpdata/gpdatap)
-MASTER_HOSTNAME=master
-MASTER_DIRECTORY=/home/$USERNAME/gpdata/gpmaster
-MASTER_PORT=5432
-TRUSTED_SHELL=ssh
-CHECK_POINT_SEGMENTS=8
-ENCODING=UNICODE
-MIRROR_PORT_BASE=50000
-REPLICATION_PORT_BASE=41000
-MIRROR_REPLICATION_PORT_BASE=51000
-declare -a MIRROR_DATA_DIRECTORY=(/home/$USERNAME/gpdata/gpdatam)
-EOF
+    SAVEDSTTY=`stty -g`
+    stty -echo
+    stty cbreak
+    dd if=/dev/tty bs=1 count=1 2> /dev/null
+    stty -raw
+    stty echo
+    stty $SAVEDSTTY
 }
+
 
 #################################################################
 ## Welcome                                                     ##
@@ -240,16 +194,16 @@ echo "************************************************************************"
 echo ""
 echo ""
 echo "CTRL+C to break command bash..." #but it seems never breaking
-
+echo $CLEAN
 #################################################################
 ## check segment file && host file                             ##
 #################################################################
-if [ ! -f /opt/all_segment ]; then
+if [ ! -f $SEGFILE ]; then
   echo "[FATAL]:-Segment file does not exist."
   exit 1
 fi
 
-if [ ! -f /opt/all_host ]; then
+if [ ! -f $HOSTFILE ]; then
   echo "[FATAL]:-Host file does not exist."
   exit 2
 fi
@@ -290,10 +244,10 @@ else if [ $MIRRORFLAG == "y" ] ; then
 fi
 
 #################################################################
-## ssh test                                                    ##
+##    ping                                                     ##
 #################################################################
 echo "[INFO]:-Running routing test..."
-for line in $(cat /opt/all_segment)
+for line in $(cat $SEGFILE)
 do
   echo "[INFO]:-Connecting $line..."
   ping -c 2 -i 0.5 $line >/dev/null && echo "[INFO]:-Success to connect $line." || ( exit 3 && echo "[FATAL]:-FAIL to connect $line. Please check the network." );
@@ -302,118 +256,47 @@ done
 #################################################################
 ## clean data directory                                        ##
 #################################################################
+
+if [ $CLEAN == "y" ] ; then
 echo "[INFO]:-Cleaning data directories at master..."
 rm -rf $MASDIR >/dev/null
 rm -rf $PTH >/dev/null
 rm -rf /home/$USERNAME/gpinitsystem_config
-
-for line in $(cat /opt/all_segment)
+for line in $(cat $SEGFILE)
 do
-  echo "[INFO]:-Cleaning data directories at $line..."
-  ssh -t -Tq root@$line "rm -rf $MRRDIR >/dev/null"
-  ssh -t -Tq root@$line "rm -rf $PRIDIR >/dev/null"
-  ssh -t -Tq root@$line "rm -rf $PTH    >/dev/null"
-  ssh -t -Tq root@$line "rm -rf /home/$USERNAME/gpinitsystem_config"
-  if [ $CUSTOMTAR == "true" ] ; then
-    ssh -t -Tq root@$line "rm -rf $TARNAME >/dev/null"
-  else
-    ssh -t -Tq root@$line "rm -rf /usr/local/$TARNAME >/dev/null"
-  fi
+echo "[INFO]:-Cleaning data directories at $line..."
+ssh -t -Tq root@$line "rm -rf $PRIDIR >/dev/null"
+ssh -t -Tq root@$line "rm -rf $PTH    >/dev/null"
 done
-echo "[INFO]:-Preparation work finished."
-
-echo "Press any key to continue..."
-char=`get_char`
+echo "[INFO]:-Clean work finished."
+fi
 
 #################################################################
 ## scp package                                                 ##
 #################################################################
-if [ $INSTALLMODE == "debug" ] ; then
-  echo "[DEBUG]:-TARNAME is $TARNAME."
-fi
-
-if [ $CUSTOMTAR == "true" ] ; then
-  echo "[INFO]:-The script is running in -P mode, the script will find the package at $TARNAME."
-  if [ ! -e $TARNAME ] ; then
+if [ ! -e $TARNAME ] ; then
   echo "[FATAL]:-Package does not exist."
-  exit 4
-  fi
-else 
-  echo "[INFO]:-The script is running in -p (default) mode, the script will find the package at /usr/local/$TARNAME."
-  if [ ! -e /usr/local/$TARNAME ] ; then
-  echo "[FATAL]:-Package does not exist."
-  exit 4
-  fi
+exit 4
 fi
-
-for line in $(cat /opt/all_segment)
+cp $TARNAME /usr/local
+for line in $(cat $SEGFILE)
 do
   echo "[INFO]:-Transporting package to $line..."
-if [ $CUSTOMTAR == "true" ] ; then
   scp -r $TARNAME root@$line:/usr/local
-else
-  scp -r /usr/local/$TARNAME root@$line:/usr/local/
-fi
 done
-
-#################################################################
-## config mount configuration                                  ##
-## [IMPORTANT] this config has an essential problem, you are   ##
-## recommended to modify it manually.                          ##
-#################################################################
-
-#sed -i "s/default/rw,noatime,inode64,allocsize=16m/g" /etc/fstab
-#sed -i "s/swap    rw,noatime,inode64,allocsize=16m/swap    default/g" /etc/fstab
-#echo "mount configuration has been modified as rw,noatime,inode64,allocsize=16m."
-#echo "Beware that mount configuration will take effects only after restart the system.[INFO]:-"
-
-#################################################################
-## system configuration                                        ##
-#################################################################
-
-echo "[HINT]:-BeyonDB Cluster are recommended to confirm sysctl.conf, which may leading to unexpected interrupt. Beware of this configuration."
-echo "Press any key to continue..."
-char=`get_char`
-echo "..."
-
-for line in $(cat /opt/all_host)
-do
-  #backup $line
-  #echo "[INFO]:-Configuring system parameters at $line."
-  #sysctlMod $line
-  #limitMod $line
-  secMod $line
-  echo "[INFO]:-System parameters have modified at $line."
-done
-echo "done."
 
 #################################################################
 ## decompress installation package                             ##
 #################################################################
-if [ $INSTALLMODE == "debug" ] ; then 
-  echo "[DEBUG]:-Variable TARNAME is $TARNAME."
-fi
 
-if [ $CUSTOMTAR == "true" ] ; then
-  echo "[INFO]:-Now decompressing BeyonDB in master. Wait for a few minutes..."
-  tar -zxvf $TARNAME -C /usr/local/ >> install_log.txt 2>> install_error.txt
-  sed -i 's/\/home\/gpdb/\/usr\/local\/gpdb/g' /usr/local/gpdb/$ENVIRONMENTFILE
-else
-  echo "[INFO]:-Now decompressing BeyonDB in master. Wait for a few minutes..."
-  tar -zxvf /usr/local/$TARNAME -C /usr/local >> install_log.txt 2>> install_error.txt
-  sed -i 's/\/home\/gpdb/\/usr\/local\/gpdb/g' /usr/local/gpdb/$ENVIRONMENTFILE
-fi
+echo "[INFO]:-Now decompressing BeyonDB in master. Wait for a few minutes..."
+tar -zxvf $TARNAME -C /usr/local/ >> install_log.txt 2>> install_error.txt
+chown -R $USERNAME:$USERNAME /usr/local/gpdb
 
-for line in $(cat /opt/all_segment)
+for line in $(cat $SEGFILE)
 do
   echo "[INFO]:-Now decompressing BeyonDB in $line. Wait for a few minutes..."
-if [ $CUSTOMTAR == "true" ] ; then
-  ssh -t root@$line "tar -zxvf /usr/local/$(basename $TARNAME) -C /usr/local/ >> install_log.txt 2>> install_error.txt"
-  ssh -t root@$line "sed -i 's/\/home\/gpdb/\/usr\/local\/gpdb/g' /usr/local/gpdb/$ENVIRONMENTFILE"
-else
   ssh -t root@$line "tar -zxvf /usr/local/$TARNAME -C /usr/local/ >> install_log.txt 2>> install_error.txt"
-  ssh -t root@$line "sed -i 's/\/home\/gpdb/\/usr\/local\/gpdb/g' /usr/local/gpdb/$ENVIRONMENTFILE"
-fi
   ssh -t root@$line "chown -R $USERNAME:$USERNAME /usr/local/gpdb"
 done
 echo "done."
@@ -425,35 +308,14 @@ echo "done."
 if [ ! -e "$PTH/$ENVIRONMENTFILE" ] ; then
   echo "[FATAL]:-Environment file <$ENVIRONMENTFILE> does not exist. Please check decompression path."
   exit 5
-else
-  echo "[INFO]:-Found environment file <$ENVIRONMENTFILE>, continue script processing."
 fi
 
-if [ -z "`grep "source $PTH/$ENVIRONMENTFILE" /home/$USERNAME/.bashrc`" ] ; then
-  echo "[INFO]:-Now sourcing environment variables..."
-  cat>>/home/$USERNAME/.bashrc<<EOF
-  source $PTH/$ENVIRONMENTFILE
-EOF
-else
-  echo "[INFO]:-A previous installation has detected, skip to change .bashrc."
-fi
-
+echo "[INFO]:-Build env , change ~/.bash_profile."
 if [ -z "`grep "source $PTH/$ENVIRONMENTFILE" /home/$USERNAME/.bash_profile`" ] ; then
   cat>>/home/$USERNAME/.bash_profile<<EOF
-  source $PTH/$ENVIRONMENTFILE
+source $PTH/$ENVIRONMENTFILE
 EOF
-else
-  echo "[INFO]:-A previous installation has detected, skip to change .bash_profile."
 fi
-
-if [ -z "`grep "source $PTH/$ENVIRONMENTFILE" /etc/profile`" ] ; then
-  cat>>/etc/profile<<EOF
-  source $PTH/$ENVIRONMENTFILE
-EOF
-else
-  echo "[INFO]:-A previous installation has detected, skip to change profile."
-fi
-echo "done."
 
 #################################################################
 ## mkdir                                                       ##
@@ -481,6 +343,7 @@ if [ $MIRRORFLAG == "y" ] ; then
 fi
 echo "done."
 
+
 #################################################################
 ## config gpinitsystem                                         ##
 #################################################################
@@ -500,5 +363,62 @@ fi
 echo "done."
 
 
+
+#################################################################
+## system configuration                                        ##
+#################################################################
+
+echo "[HINT]:-BeyonDB Cluster are recommended to confirm sysctl.conf, which may leading to unexpected interrupt. Beware of this configuration."
+echo "---------------------------------------------------------"
+read -p "Do you want to turn off the firewall?(y/n):" FIREWALL
+if [ $FIREWALL == 'y' ] ; then
+    for line in $(cat $HOSTFILE)
+        do
+            secMod $line
+            echo "[INFO]:-FIREWALL is closed at $line."
+        done
+    echo "done."
+ else if [ $FIREWALL != 'n' ] ; then
+ echo "Please input 'y' or 'n'!"
+ else
+    echo "[WARNING]:- Firewalls will cause communication to be blocked"
+ fi
+fi
+
+read -p "Do you want to modify the SELINUX config ?(y/n):" SELINUX
+if [ $SELINUX == 'y' ] ; then
+    for line in $(cat $HOSTFILE)
+        do
+            secMod $line
+            echo "[INFO]:-SELINUX config is modified at $line."
+        done
+    echo "done."
+ else if [ $FIREWALL != 'n' ] ; then
+ echo "Please input 'y' or 'n'!"
+ else
+    echo "[WARNING]:- SELINUX will cause communication to be blocked"
+ fi
+fi
+
+read -p "Do you want to Configuring system parameters ?(y/n):" SYSCTL
+if [ $SYSCTL == 'y' ] ; then
+    for line in $(cat $HOSTFILE)
+        do
+            echo "[INFO]:-Configuring system parameters at $line."
+            sysctlMod $line
+            limitMod $line
+            echo "[INFO]:-System parameters have modified at $line."
+        done
+    echo "done."
+else if [ $SYSCTL != 'n' ] ; then
+    echo "Please input 'y' or 'n'!"
+else
+    echo "[WARNING]:- The system may not perform at its best."
+fi
+fi
 echo "[INFO]:-Install script finished."
-echo "[HINT]:-Before run gpinitsystem -c /home/$USERNAME/gpinitsystem_config to init BeyonDB MPP, you should reboot system first."
+
+echo "[HINT]:-NOW ,RUN gpinitsystem -c /home/$USERNAME/gpinitsystem_config to init BeyonDB MPP."
+echo "Press any key to continue..."
+char=`get_char`
+su -  $USERNAME -c "gpinitsystem -c gpinitsystem_config"
